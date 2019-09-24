fe28f21dc0891e4da19b433f765136e2b2527bef
Bui Dang Khanh <khanh.buidang@homecredit.vn>
2019-09-24 17:45:31 +0700

    function done

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ let out_e = 'time'
 let ctl_dis
   , ctl_pa_m, ctl_pa_s, ctl_spd
   , ctl_ti_h, ctl_ti_m, ctl_ti_s
-  , clt_unit_dis, clt_unit_speed
+  , ctl_unit_dis, ctl_unit_speed
+
+  , [v_dis, v_pace, v_spd] = [0, 0, 0]
+  , v_time
+  , v_h, v_m, v_s
+  ;
+
 $(document).ready(function () {
   [ctl_dis
     , ctl_pa_m, ctl_pa_s, ctl_spd
     , ctl_ti_h, ctl_ti_m, ctl_ti_s
-    , clt_unit_dis, clt_unit_speed] =
+    , ctl_unit_dis, ctl_unit_speed] =
   [document.getElementById('txt-distance')
     , document.getElementById('txt-p-min'), document.getElementById('txt-p-sec'), document.getElementById('txt-spd')
     , document.getElementById('txt-t-hh'), document.getElementById('txt-t-mm'), document.getElementById('txt-t-ss')
@@ -52,40 +58,72 @@ function change_input(e){
 }
 
 function calc(){
-  let dis = +ctl_dis.value || 0
-    , pa_m = +ctl_pa_m.value || 0
-    , pa_s = +ctl_pa_s.value || 0
-    , ti_h = +ctl_ti_h.value || 0, ti_m = +ctl_ti_m.value || 0, ti_s = +ctl_ti_s.value || 0
+    // , ti_h = +ctl_ti_h.value || 0, ti_m = +ctl_ti_m.value || 0, ti_s = +ctl_ti_s.value || 0
 
   if (out_e == 'time'){
-    let pace_total = pa_m * 60 + pa_s;
-    let time_total = dis * pace_total;
+    let time_total = v_dis * v_pace;
     ctl_ti_h.value = ~~(time_total / 3600);
     ctl_ti_m.value = ~~(time_total % 3600 / 60);
     ctl_ti_s.value = time_total % 60;
   }
   else if (out_e == 'pace'){
-    let pace_total = (ti_h * 3600 + ti_m * 60 + ti_s) / dis;
+    let pace_total = v_time / v_dis;
     ctl_pa_m.value = ~~(pace_total / 60);
     ctl_pa_s.value = ~~pace_total % 60;
-    ctl_spd.value = +(3600 / pace_total).toFixed(3);
+    v_spd = 3600 / pace_total;
+    onchange_sel_spd();
   }
   else if (out_e == 'dis'){
-    let pace_total = pa_m * 60 + pa_s;
-    let time_total = ti_h * 3600 + ti_m * 60 + ti_s;
-    ctl_dis.value = +(time_total / pace_total).toFixed(3);
+    v_dis = v_time / v_pace;
+    onchange_sel_dis();
   }
 }
 
 function onchange_dis(control) {
+  v_dis = +ctl_dis.value || 0;
+  if (ctl_unit_dis.value == 'm')
+    v_dis /= 1000;
+  else if (ctl_unit_dis.value == 'mi')
+    v_dis *= 1.60934;
+
   change_input('dis');
 }
 function onchange_pa(control) {
+  v_pace = (+ctl_pa_m.value || 0) * 60 + (+ctl_pa_s.value || 0);
+  v_spd = 3600 / v_pace;
+  onchange_sel_spd();
+
   change_input('pace');
 }
 function onchange_sp(control) {
+  v_spd = (+ctl_spd.value || 0);
+  if (ctl_unit_speed.value == 'm/min')
+    v_spd *= .06;
+  else if (ctl_unit_speed.value == 'mi/h')
+    v_spd *= 1.60934;
+
+  ctl_pa_m.value = ~~(60 / v_spd);
+  ctl_pa_s.value = ~~(3600 / v_spd % 60);
   change_input('pace');
 }
 function onchange_time(control) {
+  v_time = (+ctl_ti_h.value || 0) * 3600 + (+ctl_ti_m.value || 0) * 60 + (+ctl_ti_s.value || 0);
   change_input('time');
+}
+
+function onchange_sel_spd() {
+  if (ctl_unit_speed.value == 'm/min')
+    ctl_spd.value = +(v_spd / .06).toFixed(3);
+  else if (ctl_unit_speed.value == 'mi/h')
+    ctl_spd.value = +(v_spd / 1.60934).toFixed(3);
+  else
+    ctl_spd.value = +v_spd.toFixed(3);
+}
+function onchange_sel_dis() {
+  if (ctl_unit_dis.value == 'm')
+    ctl_dis.value = +(v_dis * 1000).toFixed(3);
+  else if (ctl_unit_dis.value == 'mi')
+    ctl_dis.value = +(v_dis / 1.60934).toFixed(3);
+  else
+    ctl_dis.value = +v_dis.toFixed(3);
 }
